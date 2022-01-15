8ed6ab984ebbf3b964d4187c32361d982ae892e2
milad <milad.xsar72@gmail.com>
2022-01-15 23:55:10 +0330

    create parserExpression skipSpace and initial files

---
+++ src/code/_parser.js
@@ -0,0 +1,23 @@
+/*
+* parser syntax -> generate a javascript object Tree
+* expressions -> expression(...args)
+*/
+
+
+// parseExpressions<Recursive function> -> return javascript object {expressionStruct<object>, nextExpression<string>} 
+const { skipSpace } = require("./_skipSpace");
+const {parseApply} = require("./_parseApply");
+
+function parseExpressions(program) {
+    program = skipSpace(program);
+    let match, expr;
+    if (match = /^"([^"]*)"/.exec(program))
+        expr = { type: "value", value: match(1) }
+    else if (match = /^\d+\b/.exec(program))
+        expr = {type : "value", value : match(0)}
+    else if(match = /^[^\s()#"]+/.exec(program)) 
+        expr = {type : "word", name : match(0)}
+    else
+        throw new SyntaxError("unexpected syntax : ", program)
+    return parseApply(expr, program.slice(match(0).length))               
+}
---
+++ src/code/_skipSpace.js
@@ -0,0 +1,13 @@
+/**
+ * remove all space at start
+ */
+
+function skipSpace(str) {
+    let first = str.search(/\S/);
+    if (first === -1)
+        return "";
+    else
+        return str.slice(first)
+}
+
+module.exports.skipSpace = skipSpace
